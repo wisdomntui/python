cba9d08b11a3749216083059072d287d93bc69f4
wisdomNtui <wisdomntui@gmail.com>
2020-10-14 00:09:35 +0100

    Done with datastructures and controlstructures

---
+++ data_structures.py
@@ -0,0 +1,33 @@
+############## LISTS ######################
+
+# def set_list(message):
+#     some_list = input(message)
+#     some_list = list(some_list)
+#     return True
+
+
+# message = "Please enter a sequence of characters: "
+# my_list = [1, 2, 3, 4, 5, 5, 5, 5]
+# my_list.append(['insert', 'remove', 'extend', 'clear', 'pop', 'append'])
+# popped = my_list.pop(0)
+# print(popped, my_list.count(5), '')
+# del my_list
+# print(my_list)
+
+
+################ TUPLES #####################
+# my_tuple = (12345, 23457, 'hella'
+# print(my_tuple)
+
+
+############### SETS ########################
+set_a = {1, 3, 4, 5, 6}
+set_b = {0, 5, 2, 3, 7}
+print((set_a ^ set_b))
+
+############### DICTIONARIES #################
+my_dict = ["name", "age", "position"]
+my_dict2 = ["name", "age", "position"]
+#my_dict2["state"] = "asleep"
+
+print(my_dict is not my_dict2)
---
+++ test.py
@@ -0,0 +1,8 @@
+x = 8
+
+
+def my_name(name: int = ""):
+    print(name)
+
+
+my_name(nae=1.5)
